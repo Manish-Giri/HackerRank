b06b71827052369f1d76e56d429ea4a7da1b0fb2
manish-giri <manish.giri.me@gmail.com>
2016-07-06 15:10:51 -0400

    If else in Java

---
+++ Java/Introduction/java-if-else.java
@@ -0,0 +1,37 @@
+    import java.io.*;
+    import java.util.*;
+    import java.text.*;
+    import java.math.*;
+    import java.util.regex.*;
+
+    public class Solution {
+
+        public static void main(String[] args) {
+
+            Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();            
+            String ans="";
+            if(n%2==1){
+              ans = "Weird";
+            }
+            else{
+            
+               //Complete the code
+                if(2 <=n && n >= 5) {
+                    ans = "Not Weird";
+                }
+                
+                else if(6 <=n && n >= 20) {
+                    ans = "Weird";                
+                }
+                
+                else if( n > 20) {
+                     ans = "Not Weird";
+}
+
+                
+            }
+            System.out.println(ans);
+            
+        }
+    }
