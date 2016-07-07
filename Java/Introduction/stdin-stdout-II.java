7528ecc438d190cdfffcfe4168a0fa925b574fd4
manish-giri <manish.giri.me@gmail.com>
2016-07-07 17:50:29 -0400

    Java Stdin Stdout II

---
+++ Java/Introduction/stdin-stdout-II.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = Integer.parseInt(scan.nextLine());
+        double d = Double.parseDouble(scan.nextLine());
+        String s = scan.nextLine();
+
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
