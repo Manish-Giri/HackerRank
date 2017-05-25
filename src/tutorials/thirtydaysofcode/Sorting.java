a6a47fd378d7f2cbd587d640e295567f786b404d
MANISH-GIRI <manish.giri.me@gmail.com>
2017-05-24 23:40:04 -0400

    Create challenge stub for Sorting

---
+++ src/tutorials/thirtydaysofcode/Sorting.java
@@ -0,0 +1,19 @@
+package tutorials.thirtydaysofcode;
+
+import java.util.Scanner;
+
+/**
+ * Created by manishgiri on 5/24/17.
+ */
+public class Sorting {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        // Write Your Code Here
+    }
+}
