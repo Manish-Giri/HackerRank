f1ede643e91489bbbdbb4fc9c7a79bf480f0ae3a
MANISH-GIRI <manish.giri.me@gmail.com>
2017-07-04 23:35:53 -0400

    Solve sorting

--- src/tutorials/thirtydaysofcode/Sorting.java
+++ src/tutorials/thirtydaysofcode/Sorting.java
@@ -7,6 +7,33 @@ import java.util.Scanner;
  */
 public class Sorting {
 
+    private static int bubbleSort(int[] numbers) {
+        int numberOfSwaps = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            // Track number of elements swapped during a single array traversal
+             // numberOfSwaps = 0;
+
+            for (int j = 0; j < numbers.length - 1; j++) {
+                // Swap adjacent elements if they are in decreasing order
+                if (numbers[j] > numbers[j + 1]) {
+                    // swap(numbers[j], numbers[j + 1]);
+                    int temp = numbers[j+1];
+                    numbers[j+1] = numbers[j];
+                    numbers[j] = temp;
+                    numberOfSwaps++;
+                }
+            }
+        }
+
+        return numberOfSwaps;
+    }
+
+    private static void swap(int num1, int num2) {
+        int temp = num2;
+        num2 = num1;
+        num1 = temp;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
@@ -15,5 +42,12 @@ public class Sorting {
             a[a_i] = in.nextInt();
         }
         // Write Your Code Here
+        int swaps = bubbleSort(a);
+
+
+        System.out.println("Array is sorted in " + swaps +" swaps.");
+        System.out.println("First Element: " + a[0]);
+        System.out.println("Last Element: " + a[a.length - 1]);
+
     }
 }
