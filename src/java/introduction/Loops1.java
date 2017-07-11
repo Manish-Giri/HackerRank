912a806d2faf0d06a6626971f70410c1ff7e6d48
MANISH-GIRI <manish.giri.me@gmail.com>
2017-07-10 20:43:08 -0400

    Add Java Loops I

---
+++ src/java/introduction/Loops1.java
@@ -0,0 +1,16 @@
+package java.introduction;
+
+import java.util.Scanner;
+
+/**
+ * Created by manishgiri on 7/10/17.
+ */
+public class Loops1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(N + " x " + i + " = " + (N * i));
+        }
+    }
+}
