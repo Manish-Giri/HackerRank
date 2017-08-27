58bee70f5eff9fadb831bb146e435ec0dfdda6c6
MANISH-GIRI <manish.giri.me@gmail.com>
2017-08-27 19:21:54 -0400

    Solve Java Output Formatting

---
+++ src/java/introduction/OutputFomatting.java
@@ -0,0 +1,21 @@
+package java.introduction;
+
+import java.util.Scanner;
+
+public class OutputFomatting {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++){
+            String s1=sc.next();
+            int x=sc.nextInt();
+            //Complete this line
+            System.out.printf("%-14s %03d", s1, x);
+            System.out.println();
+        }
+        System.out.println("================================");
+
+    }
+
+}
