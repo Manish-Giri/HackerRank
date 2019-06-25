cff3c4e19751756823fd8d1e33092df2e80fb6e0
manish-giri <manish.giri.me@gmail.com>
2019-06-25 02:23:12 -0400

    Solve Arrays-DS

---
+++ src/domains/datastructures/arrays/ArrayDS.java
@@ -0,0 +1,56 @@
+package domains.datastructures.arrays;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class ArrayDS {
+    // Complete the reverseArray function below.
+    static int[] reverseArray(int[] a) {
+        int n = a.length-1;
+        int[] reversed = new int[n+1];
+        for(int i = n; i >= 0; i--) {
+            reversed[n-i] = a[i];
+        }
+        return reversed;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arrCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[arrCount];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arrCount; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int[] res = reverseArray(arr);
+
+        for (int i = 0; i < res.length; i++) {
+            bufferedWriter.write(String.valueOf(res[i]));
+
+            if (i != res.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+
+}
