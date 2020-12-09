3451f16119ac3ba7ccc5cf8b0a275ff4df6a300c
Manish-Giri <manish.giri.me@gmail.com>
2020-12-09 18:40:05 -0500

    More Basic Select queries, update .log gitignore

---
+++ SQL/subdomains/basic-select/japanese-city-attributes.sql
@@ -0,0 +1,5 @@
+-- Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+
+SELECT *
+FROM CITY
+WHERE COUNTRYCODE='JPN'
---
+++ SQL/subdomains/basic-select/weather-obs-stn3.sql
@@ -0,0 +1,5 @@
+-- Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE MOD(ID,2)=0;
