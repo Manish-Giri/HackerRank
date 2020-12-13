9efd662173d6075e5d6e37b92511cfc95f3f3953
Manish-Giri <manish.giri.me@gmail.com>
2020-12-12 20:33:19 -0500

    Weather Observation Station 10

---
+++ SQL/subdomains/basic-select/weather-obs-stn10.sql
@@ -0,0 +1,10 @@
+-- Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+
+/*
+Enter your query here.
+*/
+
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY REGEXP '.+[^aeiou]$';
