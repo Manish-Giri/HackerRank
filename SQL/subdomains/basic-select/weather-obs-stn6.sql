0eac6abdf77e57a549e9da69b245dc835a7c07c6
Manish-Giri <manish.giri.me@gmail.com>
2020-12-12 20:15:31 -0500

    Weather Observation Station 6

---
+++ SQL/subdomains/basic-select/weather-obs-stn6.sql
@@ -0,0 +1,6 @@
+-- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY REGEXP '^[aeiou]+';
